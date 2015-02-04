630e0780f036790e735ba5d8c0e00f03563c32eb
Fatimah Zohra <fzohra92@utexas.edu>
2015-02-04 00:51:29 -0600

    Combined Collatz.py and RunCollatz.py into SphereCollatz.py to make submission to Sphere Online Judge

---
+++ SphereCollatz.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# ------------------------------
+# SphereCollatz.py
+# Fatimah Zohra
+# ------------------------------
+
+# -------
+# imports
+# -------
+
+import sys
+
+# ------------
+# collatz_read
+# ------------
+
+def collatz_read (s) :
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval (i, j) :
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+    assert i > 0 and i < 1000000
+    assert j > 0 and j < 1000000
+
+    if i > j:
+        temp = i
+        i = j
+        j = temp
+
+    m = 1  #initialize a max variable
+
+    for n in range(i, j+1):
+        c = 1 #initialize a count variable
+        while n != 1:
+            if n % 2 == 0:
+                n = n/2
+            else:
+                n = 3*n +1
+            c += 1
+        m = max(c, m)
+    return m
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print (w, i, j, v) :
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve (r, w) :
+    """
+    r a reader
+    w a writer
+    """
+    for s in r :
+        i, j = collatz_read(s)
+        v    = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    collatz_solve(sys.stdin, sys.stdout)
+
