1c1ef1cca5483022e137796a9266c588cc753d2a
Fatimah Zohra <fzohra92@utexas.edu>
2015-02-03 22:57:02 -0600

    Added additional test cases for collatz_eval(), collatz_read(), collatz_print(), collatz_solve() in TextCollatz.py in order to better define the expected behavior of the collatz program

--- TestCollatz.py
+++ TestCollatz.py
@@ -30,6 +30,25 @@ class TestCollatz (TestCase) :
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
 
+    def test_read_1 (self) :
+        s    = "1 1\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 1)
+        self.assertEqual(j, 1)
+
+    #TODO: Should we only expect invalid input. Soft fail invalid inputs (out of range integers) maybe?
+    def test_read_2 (self) :
+        s    = "1 -10\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  1)
+        self.assertEqual(j, -10)
+
+    def test_read_3 (self) :
+        s    = "1 0\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  1)
+        self.assertEqual(j, 0)
+
     # ----
     # eval
     # ----
@@ -50,14 +69,37 @@ class TestCollatz (TestCase) :
         v = collatz_eval(900, 1000)
         self.assertEqual(v, 174)
 
+    def test_eval_5 (self) :
+        v = collatz_eval(1, 1)
+        self.assertEqual(v, 1)
+
+    def test_eval_6 (self) :
+        v = collatz_eval(10, 1)
+        self.assertEqual(v, 20)
+
     # -----
     # print
     # -----
-"""
+
     def test_print (self) :
         w = StringIO()
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
+
+    def test_print_1 (self) :
+        w = StringIO()
+        collatz_print(w, 1, 1, 1)
+        self.assertEqual(w.getvalue(), "1 1 1\n")
+
+    def test_print_2 (self) :
+        w = StringIO()
+        collatz_print(w, 0, 0, 0)
+        self.assertEqual(w.getvalue(), "0 0 0\n")
+
+    def test_print_3 (self) :
+        w = StringIO()
+        collatz_print(w, 1, -10, 20)
+        self.assertEqual(w.getvalue(), "1 -10 20\n")
 
     # -----
     # solve
@@ -67,7 +109,16 @@ class TestCollatz (TestCase) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
-"""
+
+    def test_solve_1 (self) :
+        r = StringIO("1 1\n1 2\n2 3\n4 3\n")
+        w = StringIO()
+        collatz_solve(r, w)
+
+    def test_solve_2 (self) :
+        r = StringIO("0 1\n-1 2\n2 3\n3 4\n")
+        w = StringIO()
+        collatz_solve(r, w)
 
 # ----
 # main
