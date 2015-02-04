0b66eff779dc03997278448aeb770c2c519e67b1
Fatimah Zohra <fzohra92@utexas.edu>
2015-02-03 21:42:55 -0600

    Fixed basic unit tests so the assertions are now failing. Need to write the Collatz core program now.

--- TestCollatz.py
+++ TestCollatz.py
@@ -36,24 +36,24 @@ class TestCollatz (TestCase) :
 
     def test_eval_1 (self) :
         v = collatz_eval(1, 10)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 20)
 
     def test_eval_2 (self) :
         v = collatz_eval(100, 200)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 125)
 
     def test_eval_3 (self) :
         v = collatz_eval(201, 210)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 89)
 
     def test_eval_4 (self) :
         v = collatz_eval(900, 1000)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 174)
 
     # -----
     # print
     # -----
-
+"""
     def test_print (self) :
         w = StringIO()
         collatz_print(w, 1, 10, 20)
@@ -67,7 +67,7 @@ class TestCollatz (TestCase) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
-
+"""
 
 # ----
 # main
